4df39e438bb90363a10e1b5746a1f3979a98dae7
Fabian Frickmann <fabian.frckmnn@gmail.com>
2021-08-19 19:27:37 +0200

    Add files via upload

---
+++ STARTSEITE.py
@@ -0,0 +1,18 @@
+# ERÖFFNET EINE SEITE
+import tkinter as TITELSEITE
+
+window = TITELSEITE.Tk()
+
+# SETZEN DER FENSTERGRÖßE
+window.geometry("1920x1080")
+# SETZT DEN FENSTERTITEL
+window.title("STARTSEITE")
+
+# ÜBERSCHRIFT HINZUFÜGEN
+titelseite_label = TITELSEITE.Label(window, text="AFS SCHÄDLINGSBEKÄMPFUNG SOFTWARE", fg='#fff000', bg='grey', font=( 'times', 25, 'bold', 'italic'), width=100, heigh=2)
+titelseite_label.place (x=5,y=5)
+# BUTTON HINZUFÜGEN
+titelseite_button = TITELSEITE.Button(window, text="BEENDEN",fg='#2F4F4F', bg='orange', width=20, heigh=5, command=window.quit)
+titelseite_button.place(x=900, y=980)
+
+window.mainloop()
